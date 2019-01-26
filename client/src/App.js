f740463a09f633d155a25f7f9cde2d2b173f06c9
csilva <csilva@silvatech.org>
2019-01-26 15:32:32 -0600

    Updated Travis file to indicate new app and env for aws. Since old App and Env was deleted since it was showing error repeatedly. Also updated AWS Run file to support 512. Updated App js to display fib calc.

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,7 @@ class App extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              Learn React
+              Fib Calc React
             </a>
           </header>
         </div>
